090e2ea0c7615987f153ac75dc260aafa81c7133
LudoCruchot <32196302+LudoCruchot@users.noreply.github.com>
2018-02-14 00:22:33 +0100

    API

---
+++ docs/scraping/lafourchette.js
@@ -0,0 +1,6 @@
+// API La Fourchette https://m.lafourchette.com/api/restaurant-prediction?name= + Nom resto ex: Auberge_duCheval
+
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
